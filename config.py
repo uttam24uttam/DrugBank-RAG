228afb0bbfd3339409118573a89e952a6e223bf7
Uttam <uttamhamsaraj24@gmail.com>
2025-11-26 22:17:23 +0530

    update

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DB_FOLDER = "chroma_db_data"
 # Model Settings
 LLM_MODEL_NAME = "utt24/qwen_1.5B_Bayesian"
 EXTRACTOR_MODEL_NAME = "Qwen/Qwen2-1.5B-Instruct"
-EXTRACTOR_USE_FINE_TUNED = True   #if True → use EXTRACTOR_MODEL_NAME
+EXTRACTOR_USE_FINE_TUNED = True   
 QUESTION_CLASSIFIER_PATH = "/content/drive/MyDrive/DrugBank-RAG/drug_bank_bert_classifier_final"
 
 # BERT  Lables
